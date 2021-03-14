08e97f2b2a4e3c5d1f7cd94189fbd4f007af0e11
Owen Luu <owenluu111@gmail.com>
2021-03-13 18:56:47 -0600

    added an index and a show

---
+++ src/views/ListingIndex.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <h1>All Listings</h1>
+    <div v-for="listing in listings" v-bind:key="listing.id">
+      <h2>{{ listing.title }}</h2>
+      <img v-bind:src="listing.image" v-bind:alt="listing.image" />
+      <p>{{ listing.description }}</p>
+      <p>{{ listing.price }}</p>
+
+      <!-- <p>Width: {{ .width }}</p>
+      <p>Height: {{ photo.height }}</p> -->
+    </div>
+  </div>
+</template>
+
+<style>
+/* img {
+  width: 250px;
+} */
+</style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      listings: [],
+      title: "",
+      price: "",
+      description: "",
+      image: "",
+    };
+  },
+  created: function() {
+    this.indexListings();
+  },
+  methods: {
+    indexListings: function() {
+      axios.get("/api/listings").then(response => {
+        console.log("All Listings", response);
+        this.listings = response.data;
+      });
+    },
+  },
+};
+</script>
---
+++ src/views/ListingShow.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>{{ listing.title }}</h2>
+    <p>{{ product.price }}</p>
+    <p>{{ product.description }}</p>
+    <a v-bind:href="`/listings/${listing.id}/edit`">Edit This Listing</a>
+    <br />
+    <a href="/listings">Back to All Listings</a>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      listings: {},
+    };
+  },
+  created: function() {
+    this.showListings();
+  },
+  methods: {
+    showListing: function() {
+      axios.get("/api/listings/" + this.$route.params.id).then(response => {
+        console.log(response.data);
+        this.listing = response.data;
+      });
+    },
+  },
+};
+</script>

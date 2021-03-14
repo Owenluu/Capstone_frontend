86091caca12e7c4075bd1e772c8cc32246656bf5
Owen Luu <owenluu111@gmail.com>
2021-03-13 19:12:29 -0600

    added cards for index

--- src/views/ListingIndex.vue
+++ src/views/ListingIndex.vue
@@ -1,22 +1,27 @@
 <template>
   <div class="home">
     <h1>All Listings</h1>
-    <div v-for="listing in listings" v-bind:key="listing.id">
-      <h2>{{ listing.title }}</h2>
-      <img v-bind:src="listing.image" v-bind:alt="listing.image" />
-      <p>{{ listing.description }}</p>
-      <p>{{ listing.price }}</p>
-
-      <!-- <p>Width: {{ .width }}</p>
-      <p>Height: {{ photo.height }}</p> -->
+    <div class="row row-cols-1 row-cols-md-2">
+      <div class="col mb-4" v-for="listing in listings" v-bind:key="listing.id">
+        <div class="card">
+          <img v-bind:src="listing.image" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">{{ listing.title }}</h5>
+            <p class="card-text">{{ listing.price }}</p>
+            <p class="card-text">{{ listing.description }}</p>
+            <a class="btn btn-primary" v-bind:href="`/listings.${listing.id}`">More Info</a>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style>
-/* img {
-  width: 250px;
-} */
+.card-img-top {
+  object-fit: cover;
+  height: 350px;
+}
 </style>
 
 <script>

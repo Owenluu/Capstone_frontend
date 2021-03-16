16f1b1ecc7b29c89f3a6f1f28e46c51ec52183a8
Owen Luu <owenluu111@gmail.com>
2021-03-16 00:13:58 -0500

    added a template and made changes to the index template

--- src/views/ListingIndex.vue
+++ src/views/ListingIndex.vue
@@ -1,6 +1,96 @@
 <template>
   <div class="home">
-    <h1>All Listings</h1>
+    <div class="slider-area">
+      <div class="slider-active owl-carousel nav-style-1  owl-dot-none">
+        <div
+          class="single-slider-2 slider-height-2 d-flex align-items-center bg-img"
+          style="background-image:url(assets/img/slider/bred11.jpg);"
+        >
+          <div class="container">
+            <div class="row">
+              <div class="col-xl-6 col-lg-7 col-md-8 col-12 ml-auto">
+                <div class="slider-content-3 slider-animated-1 text-center">
+                  <h3 class="animated" style="color:white">Buy & Sell</h3>
+                  <h1 class="animated" style="color:white">The Latest Heat</h1>
+                  <p class="animated"></p>
+                  <div class="slider-btn btn-hover">
+                    <a class="animated" style="color:white" href="shop.html">SHOP NOW</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="single-slider-2 slider-height-2 d-flex align-items-center bg-img"
+          style="background-image:url(assets/img/slider/OFW5.jpg);"
+        >
+          <div class="container">
+            <div class="row">
+              <div class="col-xl-6 col-lg-7 col-md-8 col-12 ml-auto">
+                <div class="slider-content-3 slider-animated-1 text-center">
+                  <h3 class="animated" style="color:black">Always</h3>
+                  <h1 class="animated">Authentic</h1>
+                  <p class="animated" style="color:black"></p>
+                  <div class="slider-btn btn-hover">
+                    <a class="animated" style="color:black" href="shop.html">SHOP NOW</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <h1></h1>
+    <div class="product-area pb-60 section-padding-1">
+      <div class="container-fluid">
+        <div class="section-title-2 text-center mb-60">
+          <h2>All Listings</h2>
+          <p>Most Popular</p>
+        </div>
+        <div class="custom-row">
+          <div v-for="listing in listings" v-bind:key="listing.id" class="custom-col-5">
+            <div class="product-wrap-2 mb-25 scroll-zoom">
+              <div class="product-img">
+                <a href="product-details.html">
+                  <img class="default-img" v-bind:src="listing.images[0].image_url" alt="" />
+                  <img class="hover-img" v-bind:src="listing.images[0].image_url" alt="" />
+                </a>
+                <span class="pink"></span>
+                <div class="product-action-2">
+                  <!-- <a title="Add To Cart" href="#"><i class="fa fa-shopping-cart"></i></a> -->
+                  <a
+                    title="Quick View"
+                    v-bind:href="`/listings/${listing.id}`"
+                    data-toggle="modal"
+                    data-target="wishlist.html"
+                  >
+                    <i class="fa fa-eye"></i>
+                  </a>
+                  <!-- <a title="Compare" href="#"><i class="fa fa-retweet"></i></a> -->
+                </div>
+              </div>
+              <div class="product-content-2">
+                <div class="title-price-wrap-2">
+                  <h3>
+                    <h5 class="card-title">{{ listing.title }}</h5>
+                    <p class="card-text">{{ listing.description }}</p>
+                  </h3>
+                  <div class="price-2">
+                    <span class="card-text">${{ listing.price }}</span>
+                    <span class="old"></span>
+                  </div>
+                </div>
+                <div class="pro-wishlist-2">
+                  <a title="Wishlist" href="wishlist.html"><i class="fa fa-heart-o"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <div class="row row-cols-1 row-cols-md-2">
       <div class="col mb-4" v-for="listing in listings" v-bind:key="listing.id">
         <div class="card">
--- src/views/ListingShow.vue
+++ src/views/ListingShow.vue
@@ -3,7 +3,10 @@
     <h2>{{ listing.title }}</h2>
     <p>{{ listing.description }}</p>
     <p>{{ listing.price }}</p>
-    <p>{{ listing.image }}</p>
+    <div v-for="image in listing.images" v-bind:key="image.id">
+      <!-- <p>{{ image.image_url }}</p> -->
+      <img src="image.image_url" alt="image of a sneaker" />
+    </div>
 
     <a v-bind:href="`/listings/${listing.id}/edit`">Edit This Listing</a>
     <br />
@@ -11,16 +14,19 @@
   </div>
 </template>
 
+<style scoped>
+img {
+  object-fit: cover;
+  height: 401px;
+}
+</style>
+
 <script>
 import axios from "axios";
 export default {
   data: function() {
     return {
       listing: {},
-      // title: "",
-      // price: "",
-      // description: "",
-      // image: "",
     };
   },
   created: function() {
@@ -28,7 +34,7 @@ export default {
   },
   methods: {
     showListing: function() {
-      // console.log("whatever");
+      // console.log("Whatever");
       axios.get("/api/listings/" + this.$route.params.id).then(response => {
         console.log(response.data);
         this.listing = response.data;
